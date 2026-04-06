25e2bbef3b06aed83d8bd776dd546788484292de
ningccy <nning611231@gmail.com>
2026-04-06 13:00:26 +0800

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -72,7 +72,8 @@ class EconomicScore(Base):
     total_score = Column(Float)
     signal_light = Column(String(10))
 #########################################
-# NewsArticle.__table__.drop(engine, checkfirst=True)
+   # Base.metadata.create_all(bind=engine)
+NewsArticle.__table__.drop(engine, checkfirst=True)
     Base.metadata.create_all(bind=engine)
     st.sidebar.info("📌 資料庫結構已完成同步")
 except Exception as schema_e:

9c166c766d00bb38feabed619e1b387c61411647
ningccy <nning611231@gmail.com>
2026-04-07 11:06:14 +0800

    Update for_news_main.py

--- scr/for_news_main.py
+++ scr/for_news_main.py
@@ -98,7 +98,7 @@ def main():
                         link  =link,
                         source = name,
                         content = clean_content,
-                        content = article.text.strip(),
+                       # content = article.text.strip(),
                         sentiment_score = sent_score,
                         importance_score = imp_score,
                         published = entry.get('published', ''),
